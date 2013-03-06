68d51daf810258fce62d164c965f5bddf898cfd2
Ben Elsworth <elswob@gmail.com>
2013-03-06 14:33:15 +0000

    fixed CreateFiles.groovy to be 100 times quicker

--- scripts/CreateFiles.groovy
+++ scripts/CreateFiles.groovy
@@ -36,10 +36,6 @@ def createAnnoFile(gff){
 	
   	def asql = "select * from anno_data where filedata_id = "+gff+" order by type;"
     def a = sql.rows(asql)
-	
-	//get list of interpro databases
-	//def iSql = "select distinct(anno_db) from gene_anno where anno_id ~ '^IPR';";
-	//def i = sql.rows(iSql).anno_db
 
 	//add headers for file
 	outfile << "\t"
@@ -53,45 +49,56 @@ def createAnnoFile(gff){
   	}
   	outfile << "\n"
   	def count = 0
-  	gffInfo.gene.sort({it.mrna_id}).each{g->
-  		count++
-  		def gMap = [:]
-  	  	a.each{anno->
-  	  		if (anno.type == 'blast'){
-				if (g.gblast.findAll({it.anno_db == anno.source})){
-					def top = g.gblast.findAll({it.anno_db == anno.source}).sort({-it.score})[0]
-					gMap."${anno.source}" = [top.anno_id,top.descr,top.score]
-				}else{
-					gMap."${anno.source}" = ["","",""]
-				}
-			}else if (anno.type == 'fun'){
-				if (g.ganno.findAll({it.anno_db == anno.source})){
-					def top = g.ganno.findAll({it.anno_db == anno.source}).sort({-it.score})[0]
-					gMap."${anno.source}" = [top.anno_id,top.descr,top.score]
-				}else{
-					gMap."${anno.source}" = ["","",""]
-				}
-			}else if (anno.type == 'ipr'){
-				if (g.ginter.findAll()){
-					def top = g.ginter.findAll().sort({it.score})[0]
-					gMap."${anno.source}" = [top.anno_id,top.descr,top.score]
-				}else{
-					gMap."${anno.source}" = ["","",""]
-				}
-			}
-      	}
-      	if ((count % 1000) ==  0){
-			println count
-			//cleanUpGorm()
+  	
+  	//create gene list
+  	def glist = [:]
+  	def gsql = "select distinct(mrna_id) mrna_id from gene_info where file_id = "+gff+" order by mrna_id;"
+  	def g = sql.rows(gsql)
+  	g.each{
+  		glist."${it.mrna_id}" = []
+  	}
+  	//get blasts
+  	a.each{anno->
+  		def bsql
+  		if (anno.type == 'blast'){
+			println "--- Getting blast data for '"+anno.source+"' ---"
+			bsql = "select distinct on (mrna_id) mrna_id,anno_id,descr,score from gene_info left outer join gene_blast on (gene_info.id = gene_blast.gene_id) where gene_info.file_id = "+gff+" and gene_blast.anno_db = '"+anno.source+"' group by mrna_id,anno_id,descr,score order by mrna_id,score;";
+		}else if (anno.type == 'fun'){
+			println "--- Getting functional annotation data for '"+anno.source+"' ---"
+			bsql = "select distinct on (mrna_id) mrna_id,anno_id,descr,score from gene_info left outer join gene_anno on (gene_info.id = gene_anno.gene_id) where gene_info.file_id = "+gff+" and gene_anno.anno_db = '"+anno.source+"' group by mrna_id,anno_id,descr,score order by mrna_id,score;";
+		}else if (anno.type == 'ipr'){
+			println "--- Getting interproscan data for '"+anno.source+"' ---"
+			bsql = "select distinct on (mrna_id) mrna_id,anno_id,descr,score from gene_info left outer join gene_interpro on (gene_info.id = gene_interpro.gene_id) where gene_info.file_id = "+gff+" group by mrna_id,anno_id,descr,score order by mrna_id,score;";
 		}
-      	//println "m = "+g.mrna_id
-      	outfile << g.mrna_id
-      	gMap.each{
-      		//println it.key+" -> "+it.value[0]+"\t"+it.value[1]+"\t"+it.value[2]+"\n"
-      		outfile << "\t"+it.value[0]+"\t"+it.value[1]+"\t"+it.value[2]
-      	}
-      	outfile << "\n"
-    }
+  		def b = sql.rows(bsql)
+  		def hit = [:]
+  		b.each{
+  			//catch the interproscan data
+  			if (it.anno_id != null){
+				hit."${it.mrna_id}" = true  
+				glist."${it.mrna_id}".add(it.anno_id)
+				glist."${it.mrna_id}".add(it.descr)
+				glist."${it.mrna_id}".add(it.score)
+			}
+  		}
+  		//add spaces for no hits
+  		glist.each{
+  			if (hit."${it.key}" != true){
+  				glist."${it.key}".add("")
+  				glist."${it.key}".add("")
+  				glist."${it.key}".add("")
+  			}
+  			
+  		}
+  	}
+  	glist.each{
+  		outfile << it.key
+  		for (i in it.value){
+  			outfile << "\t"+i
+  		} 
+  		outfile << "\n"
+  	}
+    
     println "Zipping up for download..."
 	def ant = new AntBuilder()
 	ant.zip(destfile: "data/"+gffInfo.file_dir+"/"+gffInfo.file_name+".anno.tsv.zip", basedir: "data/"+gffInfo.file_dir, includes: gffInfo.file_name+".anno.tsv")
